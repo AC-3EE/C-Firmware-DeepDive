c0f9bfcfed5678fdf0b9ed00fbae0a8058386445
AC-3EE <66760632+AC-3EE@users.noreply.github.com>
2025-07-09 13:05:59 +0800

    feat: 新增靜態變數

--- 01-core-and-memory/01-memory-layout/main.c
+++ 01-core-and-memory/01-memory-layout/main.c
@@ -4,17 +4,19 @@
 #include <stdlib.h>
 
 // --- .Data Section ---
-// g_initialized_var 是一個已初始化的全域變數。
-// 它會被儲存在 .data 區段。
+// 已初始化的全域變數
 int g_initialized_var = 10;
+// NEW: 已初始化的靜態全域變數，同樣儲存在 .data 區段
+static int s_initialized_var = 11; 
 
 // --- .BSS Section ---
-// g_uninitialized_var 是一個未初始化的全域變數。
-// C語言標準保證它會被初始化為0，並儲存在 .bss 區段，以節省執行檔大小。
+// 未初始化的全域變數
 int g_uninitialized_var;
+// NEW: 未初始化的靜態全域變數，同樣儲存在 .bss 區段
+static int s_uninitialized_var;
 
 // --- .Text Section ---
-// main 函式本身是一段程式碼，它的機器指令會被儲存在 .text 區段。
+// 函式本身是一段程式碼
 void print_memory_layout() {
     printf("這是一個在 .text 區段的函式。\n");
 }
@@ -22,13 +24,11 @@ void print_memory_layout() {
 
 int main() {
     // --- .Stack Section ---
-    // l_local_var 是 main 函式內的區域變數。
-    // 它會被儲存在 stack (堆疊) 中。
+    // 區域變數
     int l_local_var = 20;
 
     // --- .Heap Section ---
-    // 使用 malloc 動態配置的記憶體。
-    // 這塊記憶體的位址來自 heap (堆積)。
+    // 動態配置的記憶體
     int *heap_var_ptr = (int*) malloc(sizeof(int));
     if (heap_var_ptr == NULL) {
         perror("malloc failed");
@@ -36,22 +36,28 @@ int main() {
     }
     *heap_var_ptr = 30;
 
-
-    // --- .Text Section (Constant strings) ---
-    // "Hello, Memory Layout!" 這樣的字串常數，通常也被儲存在 .text 區段，
-    // 或者是另一個唯讀資料區段 (rodata)。
+    // --- .rodata Section (Constant strings) ---
+    // 字串常數
     const char *string_literal = "Hello, Memory Layout!";
 
 
     printf("==================== Memory Addresses ====================\n");
+    // .Text: 函式指標
     printf("Address of a function (Text)         : %p\n", &print_memory_layout);
+    // .rodata: 唯讀的字串常數
     printf("Address of a string literal (rodata) : %p\n", (void*)string_literal);
     printf("----------------------------------------------------------\n");
+    // .Data: 已初始化的全域/靜態變數
     printf("Address of initialized global (Data) : %p\n", &g_initialized_var);
+    printf("Address of initialized static (Data) : %p\n", &s_initialized_var); // NEW
+    // .BSS: 未初始化的全域/靜態變數
     printf("Address of uninitialized global (BSS): %p\n", &g_uninitialized_var);
+    printf("Address of uninitialized static (BSS): %p\n", &s_uninitialized_var); // NEW
     printf("----------------------------------------------------------\n");
+    // .Heap: 動態配置的記憶體空間
     printf("Address of heap variable (Heap)      : %p\n", (void*)heap_var_ptr);
     printf("----------------------------------------------------------\n");
+    // .Stack: 函式內宣告的區域變數
     printf("Address of local variable (Stack)    : %p\n", &l_local_var);
     printf("==========================================================\n");
 
